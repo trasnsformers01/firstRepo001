eb76501441048bbf0b7b1d7b9f34c64bb85d2496
transformers <daud.amin@tekschool.us>
2023-03-01 19:29:27 -0500

    newly added changes

--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -5,10 +5,13 @@ public class UnitTest {
 	
 	public static void main (String []args) {
 		
-		int a = 10;
-		int b = 15;
-		int c = a+b;
-		System.out.println (c);
+
+
+		System.out.println (testOne(10, 5));
 		
+		
+	}
+	public static int testOne (int a, int b) {
+		return a-b;
 	}
 }
